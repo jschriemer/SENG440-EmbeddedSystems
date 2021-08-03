7254623bf35fa2f33360c03a8e7ca598c18a0545
andrew polanyi <andrew.polanyi@gmail.com>
2021-08-03 12:02:26 -0700

    added multiple photos showing the errors in photos, and fixed the error

--- imgrw.c
+++ imgrw.c
@@ -6,13 +6,21 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
 
-#define FLOAT_TO_INT(x) ((x)>=0?(int)((x)+0.5):(int)((x)-0.5))
+#define FLOAT_TO_INT(x) ((x)>=0?(uint8_t)((x)+0.5):(uint8_t)((x)-0.5))
+
+static float Min(float a, float b) {
+	return a <= b ? a : b;
+}
+
+static float Max(float a, float b) {
+	return a >= b ? a : b;
+}
 
 int main(void){
 
     
     int width, height, channels;        //channels represent rgb and a for opacity
-    unsigned char *img = stbi_load("test.jpg", &width,&height,&channels, 0);
+    unsigned char *img = stbi_load("bird.jpg", &width,&height,&channels, 0);
     if (img == NULL){
         printf("Error, no image found.\n");
         exit(1);
@@ -32,15 +40,15 @@ int main(void){
     //modify pixel values;  
     for (unsigned char *i = img, *ni = new_img; i !=img+img_size; i +=channels, ni +=channels){
         
-        *ni = (uint8_t) FLOAT_TO_INT((16.0 + 0.257 * (*i))) + FLOAT_TO_INT((0.504 * (*(i+1)) )) + FLOAT_TO_INT((0.098 * (*(i+2))));        //Y
-        *(ni+1) = (uint8_t) FLOAT_TO_INT((128.0 - 0.148 * (*i))) - FLOAT_TO_INT((0.291 * (*(i+1)))) + FLOAT_TO_INT((0.439 * (*(i+2))));    //Cb
-        *(ni+2) = (uint8_t) FLOAT_TO_INT((128.0 + 0.439 * (*i))) - FLOAT_TO_INT((0.368 * (*(i+1)))) - FLOAT_TO_INT((0.071 * (*(i+2))));    //Cr
+        *ni = (uint8_t) FLOAT_TO_INT((0.299 * (*i)) + (0.587 * (*(i+1))) + (0.114 * (*(i+2))));        //Y
+        *(ni+1) = (uint8_t) FLOAT_TO_INT((128.0 - 0.169 * (*i)) - (0.331 * (*(i+1))) + (0.500 * (*(i+2))));    //Cb
+        *(ni+2) = (uint8_t) FLOAT_TO_INT((128.0 + 0.500 * (*i)) - (0.419 * (*(i+1))) - (0.081 * (*(i+2))));    //Cr
     }
 
 
     stbi_write_jpg("test_ycbcr.jpg",width,height,channels,new_img, 100);
 
-    unsigned char *ycbcr_img = stbi_load("test_ycbcr.jpg", &width,&height,&channels, 0);
+    unsigned char *ycbcr_img = new_img;
     if (ycbcr_img == NULL){
         printf("Error, no image found.\n");
         exit(1);
@@ -50,12 +58,12 @@ int main(void){
     //modify pixel values back to rgb;  
     for (unsigned char *i = ycbcr_img, *ni = rgb_img; i !=ycbcr_img+img_size; i +=channels, ni +=channels){
         
-        *ni = (uint8_t) FLOAT_TO_INT((1.164 * (*i - 16))) + FLOAT_TO_INT((1.596 * (*(i+2)-128)));                                               //R
-        *(ni+1) = (uint8_t) FLOAT_TO_INT((1.164 * (*i - 16))) - FLOAT_TO_INT((0.813 * (*(i+2)-128))) - FLOAT_TO_INT((0.391 * (*(i+1)-128)));    //G
-        *(ni+2) = (uint8_t) FLOAT_TO_INT((1.164 * (*i - 16))) + FLOAT_TO_INT((2.018 * (*(i+1)-128)));                                           //B
+        *ni = (uint8_t) Max(0.00, Min(255.0, *i + (1.4 * (*(i+2)-128))));                                               //R
+        *(ni+1) =(uint8_t) Max(0.00, Min(255.0, *i - (0.711 * (*(i+2)-128)) - (0.343 * (*(i+1)-128))));    //G
+        *(ni+2) =(uint8_t) Max(0.00, Min(255.0, *i + (1.765 * (*(i+1)-128))));                                           //B
     }
     
-    stbi_write_jpg("back_to_rgb.jpg",width,height,channels,rgb_img, 100);
+    stbi_write_jpg("back_to_rgb6.jpg",width,height,channels,rgb_img, 100);
 
     stbi_image_free(img);
     free(new_img);
