67bf5241e8c589b2d9cbed8829dfcd730fc7b116
andrew polanyi <andrew.polanyi@gmail.com>
2021-08-05 10:49:38 -0700

    changed upload quality so that image does not increase in size

--- imgrw.c
+++ imgrw.c
@@ -20,7 +20,7 @@ int main(void){
 
     
     int width, height, channels;        //channels represent rgb and a for opacity
-    unsigned char *img = stbi_load("bird.jpg", &width,&height,&channels, 0);
+    unsigned char *img = stbi_load("test.jpg", &width,&height,&channels, 0);
 
     if (img == NULL){
         printf("Error, no image found.\n");
@@ -39,14 +39,14 @@ int main(void){
     //modify pixel values to form YCbCr;  
     for (unsigned char *i = img, *ni = new_img; i !=img+img_size; i +=channels, ni +=channels){
         
-        *ni = (uint8_t) FLOAT_TO_INT((0.299 * (*i)) + (0.587 * (*(i+1))) + (0.114 * (*(i+2))));        //Y
-        *(ni+1) = (uint8_t) FLOAT_TO_INT((128.0 - 0.169 * (*i)) - (0.331 * (*(i+1))) + (0.500 * (*(i+2))));    //Cb
-        *(ni+2) = (uint8_t) FLOAT_TO_INT((128.0 + 0.500 * (*i)) - (0.419 * (*(i+1))) - (0.081 * (*(i+2))));    //Cr
+        *ni = FLOAT_TO_INT((0.299 * (*i)) + (0.587 * (*(i+1))) + (0.114 * (*(i+2))));        //Y
+        *(ni+1) = FLOAT_TO_INT((128.0 - 0.169 * (*i)) - (0.331 * (*(i+1))) + (0.500 * (*(i+2))));    //Cb
+        *(ni+2) =  FLOAT_TO_INT((128.0 + 0.500 * (*i)) - (0.419 * (*(i+1))) - (0.081 * (*(i+2))));    //Cr
     }
     
     printf("Converted rgb image to YCbCr formate. \n");
 
-    stbi_write_jpg("test_ycbcr.jpg",width,height,channels,new_img, 100);
+    stbi_write_jpg("test_ycbcr.jpg",width,height,channels,new_img, 80);     
 
     printf("Wrote YCbCr image to file test_ycbcr.jpg \n");
 
@@ -60,13 +60,13 @@ int main(void){
     //modify pixel values back to rgb;  
     for (unsigned char *i = ycbcr_img, *ni = rgb_img; i !=ycbcr_img+img_size; i +=channels, ni +=channels){
         
-        *ni = (uint8_t) Max(0, Min(255, (4194304 * (*i) + (5872026 * (*(i+2)-128))) >>22));                                               //R
-        *(ni+1) =(uint8_t) Max(0, Min(255, (4194304 * (*i) - (2982150 * ((*(i+2)-128))) - (1438646 * (*(i+1)-128))) >>22));                 //G
-        *(ni+2) =(uint8_t) Max(0, Min(255, (4194304 * (*i) + (7402947 * (*(i+1)-128))) >>22));                                          //B
+        *ni =  Max(0, Min(255, (4194304 * (*i) + (5872026 * (*(i+2)-128))) >>22));                                               //R
+        *(ni+1) = Max(0, Min(255, (4194304 * (*i) - (2982150 * ((*(i+2)-128))) - (1438646 * (*(i+1)-128))) >>22));                 //G
+        *(ni+2) = Max(0, Min(255, (4194304 * (*i) + (7402947 * (*(i+1)-128))) >>22));                                          //B
     }
     printf("Converted YCbCr image back to RGB formate. \n");
 
-    stbi_write_jpg("back_to_rgb.jpg",width,height,channels,rgb_img, 100);
+    stbi_write_jpg("back_to_rgb6.jpg",width,height,channels,rgb_img, 80);
 
     printf("Wrote new RGB image to file back_to_rgb.jpg \n");
 
