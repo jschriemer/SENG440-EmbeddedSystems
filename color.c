8fd063ddef0b4fe00bfb9fdae9429532b5dd4a49
Jschriem <jschriem@uvic.ca>
2021-08-06 18:42:50 -0700

    file dump

--- color.c
+++ color.c
@@ -1,36 +1,6 @@
 #include <stdio.h>
-// function to round floating point values to nearest int
-#define FLOAT_TO_INT(x) ((x)>=0?(int)((x)+0.5):(int)((x)-0.5))
 
 int main( void) {
-        float r, g, b, y, cr, cb;
-        printf("r= ");
-        scanf("%f",&r);
-        printf("g= ");
-        scanf("%f",&g);
-        printf("b= ");
-        scanf("%f",&b);
-        int rows = 0;
-        int cols = 0;
-        //for(int i = 0; i < rows; i++){
-        //      for(int c = 0; c < cols; c++) {
-		//		... read r, g, b ...
-        //              y = 16.0 + 0.257 * r + 0.504 * g + 0.098 * b;
-        //              cb = 128.0 - 0.148 * r - 0.291 * g + 0.439 * b;
-        //              cr = 128.0 + 0.439 * r - 0.368 * g - 0.071 * b;
-        //      
-		// 		... do this conversion 4 times ...
-		//		... average four cb/cr values to do downsampling ...
-		//		}
-        //}
-        y = FLOAT_TO_INT(16.0 + 0.257 * r) + FLOAT_TO_INT(0.504 * b ) + FLOAT_TO_INT(0.098 * b);
-        cb = FLOAT_TO_INT(128.0 - 0.148 * r) - FLOAT_TO_INT(0.291 * g) + FLOAT_TO_INT(0.439 * b);
-        cr = FLOAT_TO_INT(128.0 + 0.439 * r) - FLOAT_TO_INT(0.368 * g) - FLOAT_TO_INT(0.071 * b);
-        printf("r= %d",(int)r);
-        printf(", g= %d",(int)g);
-        printf(", b= %d\n",(int)b);
-        printf("y= %d",(int)y);
-        printf(", cb= %d",(int)cb);
-        printf(", cr= %d\n",(int)cr);
-        return(0);
+	printf("hello world!");
+	return 0;
 }
--- imgrw.c
+++ imgrw.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
-
-#define FLOAT_TO_INT(x) ((x)>=0?(uint8_t)((x)+0.5):(uint8_t)((x)-0.5))
 
 static int Min(int a, int b) {
 	return a <= b ? a : b;
@@ -20,7 +17,7 @@ int main(void){
 
     
     int width, height, channels;        //channels represent rgb and a for opacity
-    unsigned char * restrict img = stbi_load("bird.jpg", &width,&height,&channels, 0);
+    unsigned char *img = stbi_load("bird.jpg", &width,&height,&channels, 0);
 
     if (img == NULL){
         printf("Error, no image found.\n");
@@ -30,18 +27,20 @@ int main(void){
     printf("Loaded rgb image. \n");
 
     size_t img_size = width * height * channels;
-    unsigned char * restrict new_img = malloc(img_size);
+    unsigned char *new_img = malloc(img_size);
     if (new_img == NULL){
         printf("unable to allocate space for new image");
         exit(1);
     }
 
+    unsigned char* i;
+    unsigned char* ni;
     //modify pixel values to form YCbCr;  
-    for (unsigned char *i = img, *ni = new_img; i !=img+img_size; i +=channels, ni +=channels){
+    for (i = img, ni = new_img; i !=img+img_size; i +=channels, ni +=channels){
         
-        *ni = (uint8_t) FLOAT_TO_INT((0.299 * (*i)) + (0.587 * (*(i+1))) + (0.114 * (*(i+2))));        //Y
-        *(ni+1) = (uint8_t) FLOAT_TO_INT((128.0 - 0.169 * (*i)) - (0.331 * (*(i+1))) + (0.500 * (*(i+2))));    //Cb
-        *(ni+2) = (uint8_t) FLOAT_TO_INT((128.0 + 0.500 * (*i)) - (0.419 * (*(i+1))) - (0.081 * (*(i+2))));    //Cr
+        *ni = ((0.299 * (*i)) + (0.587 * (*(i+1))) + (0.114 * (*(i+2))));        //Y
+        *(ni+1) = ((128.0 - 0.169 * (*i)) - (0.331 * (*(i+1))) + (0.500 * (*(i+2))));    //Cb
+        *(ni+2) = ((128.0 + 0.500 * (*i)) - (0.419 * (*(i+1))) - (0.081 * (*(i+2))));    //Cr
     }
     
     printf("Converted rgb image to YCbCr formate. \n");
@@ -50,23 +49,23 @@ int main(void){
 
     printf("Wrote YCbCr image to file test_ycbcr.jpg \n");
 
-    unsigned char * restrict ycbcr_img = new_img;
+    unsigned char *ycbcr_img = new_img;
     if (ycbcr_img == NULL){
         printf("Error, no image found.\n");
         exit(1);
     }
 
     unsigned char *rgb_img = malloc(img_size);
-    //modify pixel values back to rgb;  
-    for (unsigned char *i = ycbcr_img, *ni = rgb_img; i !=ycbcr_img+img_size-channels; i +=channels, ni +=channels){
+    //modify pixel values back to rgb; 
+    for (i = ycbcr_img, ni = rgb_img; i !=ycbcr_img+img_size-channels; i +=channels, ni +=channels){
         
-        *ni = (uint8_t) Max(0, Min(255, (4194304 * (*i) + (5872026 * (*(i+2)-128))) >>22));                                               //R
-        *(ni+1) =(uint8_t) Max(0, Min(255, (4194304 * (*i) - (2982150 * ((*(i+2)-128))) - (1438646 * (*(i+1)-128))) >>22));                 //G
-        *(ni+2) =(uint8_t) Max(0, Min(255, (4194304 * (*i) + (7402947 * (*(i+1)-128))) >>22));                                          //B
+        *ni = Max(0, Min(255, (4194304 * (*i) + (5872026 * (*(i+2)-128))) >>22));                                               //R
+        *(ni+1) = Max(0, Min(255, (4194304 * (*i) - (2982150 * ((*(i+2)-128))) - (1438646 * (*(i+1)-128))) >>22));                 //G
+        *(ni+2) = Max(0, Min(255, (4194304 * (*i) + (7402947 * (*(i+1)-128))) >>22));                                          //B
 
-        *(ni+channels) = (uint8_t) Max(0, Min(255, (4194304 * (*i+channels) + (5872026 * (*(i+2+channels)-128))) >>22));                                               //R
-        *(ni+1+channels) =(uint8_t) Max(0, Min(255, (4194304 * (*i+channels) - (2982150 * ((*(i+2+channels)-128))) - (1438646 * (*(i+1+channels)-128))) >>22));                 //G
-        *(ni+2+channels) =(uint8_t) Max(0, Min(255, (4194304 * (*i+channels) + (7402947 * (*(i+1+channels)-128))) >>22));                                          //B
+        *(ni+channels) = Max(0, Min(255, (4194304 * (*i+channels) + (5872026 * (*(i+2+channels)-128))) >>22));                                               //R
+        *(ni+1+channels) = Max(0, Min(255, (4194304 * (*i+channels) - (2982150 * ((*(i+2+channels)-128))) - (1438646 * (*(i+1+channels)-128))) >>22));                 //G
+        *(ni+2+channels) = Max(0, Min(255, (4194304 * (*i+channels) + (7402947 * (*(i+1+channels)-128))) >>22));                                          //B
     }
     printf("Converted YCbCr image back to RGB formate. \n");
 
